b486d024442ce44d172c633e97dec602edfce006
Anujit Ganguly <gangulyanujit@gmail.com>
2020-12-01 12:02:26 +0530

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,37 @@
+/*QUESTION: Write a code for insertion sort.*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,n,temp,a[100];
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    cout<<"Enter the elements:-"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    for(i=1;i<=n-1;i++)
+    {
+        temp=a[i];
+        j=i-1;
+
+        while((temp<a[j])&&(j>=0))
+        {
+            a[j+1]=a[j];    //moves element forward
+            j=j-1;
+        }
+
+        a[j+1]=temp;    //insert element in proper place
+    }
+
+    cout<<"Sorted list:-"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
