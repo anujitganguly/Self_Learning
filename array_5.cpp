741506d59a84e0ac84ce5bfd226dcb095822ab17
Anujit Ganguly <gangulyanujit@gmail.com>
2020-12-08 16:45:07 +0530

    Update array_5.cpp

--- array_5.cpp
+++ array_5.cpp
@@ -1,31 +1,29 @@
 //QUESTION: How do you find the duplicate numbers in an array if it contains multiple duplicates?
-//TRICK: Here we need two different arrays
 
-//NEEDS CORRECTION
-#include<iostream>
+#include<bits/stdc++.h>
 #define MAX 100
 using namespace std;
 
 int main()
 {
-    int n, i, j;
-    int arr[MAX], arr2[MAX];
-    cout<<"Enter size of the array: ";
-    cin>>n;
-    cout<<"Enter the elements in the array:-"<<endl;
-    for(i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    for(i=0;i<n-2;i++)
-    {
-        for(j=i+1;j<n-1;j++)
+    int arr[MAX];
+    int num, i, j, k;
+    cout<<"Enter size of array";
+    cin>>num;
+    cout<<"Enter elements of array:"<<endl;
+    for(i=0;i<num;i++)
         {
-            if(arr2[j]==0)
-            {
-                if(arr[i]==arr2[j])
-                    cout<<arr[i];
-            }
+            cin>>arr[i];
+        }
+    cout<<"Duplicate elements are: "<<endl;
+    for(i=0;i<num;i++)
+    {
+        for(j=i+1;j<num;j++)
+        {
+            if(arr[i]==arr[j])
+                {
+                    cout<<arr[i]<<endl;
+                }
         }
     }
     return 0;
